061949c8f4438999750afba41416acbed8e8b74f
Juan Idrobo <juanes453@hotmail.com>
2022-10-10 14:49:25 -0500

    Principal character animation 1

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ window.addEventListener('load', function(){
                     this.game.keys.push(e.key);
                 }else if(e.key === ' '){
                     this.game.player.shootTop();
+                } else if (e.key === 'd'){
+                    this.game.debug = !this.game.debug;
                 }
                 console.log(this.game.keys);
             });
@@ -51,9 +53,13 @@ window.addEventListener('load', function(){
             this.height = 190;
             this.x = 20;
             this.y = 100;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.maxFrame = 37;
             this.speedY = 0;
             this.maxSpeed = 2;
             this.projectiles = []
+            this.image = document.getElementById('player');
         }
         update(){
             if(this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
@@ -65,10 +71,16 @@ window.addEventListener('load', function(){
                 projectile.update();
             });
             this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
+            if (this.frameX < this.maxFrame){
+                this.frameX++;
+            } else{
+                this.frameX = 0;
+            }
         }
         draw(context){
-            context.fillStyle = 'aqua';
-            context.fillRect(this.x, this.y, this.width, this.height);
+            //context.fillStyle = 'aqua';
+            if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width , this.height, this.x, this.y, this.width, this.height)
             this.projectiles.forEach(projectile =>{
                 projectile.draw(context);
             });
@@ -214,6 +226,7 @@ window.addEventListener('load', function(){
             this.gameTime = 0;
             this.timeLimit = 5000;
             this.speed = 1;
+            this.debug = true;
         }
         update(deltaTime){
             if(!this.gameOver) this.gameTime += deltaTime;
